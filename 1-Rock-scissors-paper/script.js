9e0d59beab4128adbfa6aa1922babcc1223f0ffa
ines <inesabbes173@gmail.com>
2019-09-06 15:53:34 +0200

    Update Css en Js

--- 1-Rock-scissors-paper/script.js
+++ 1-Rock-scissors-paper/script.js
@@ -17,8 +17,57 @@ let userOption = undefined;
         userSelectionElement.style.border = "10px #990066 solid";
     }
     //Function that execute the game.
-
-   //Function that compares the two selections and return a message.
-
+    function playGame() {
+        let computerOption = Math.random();
+        if (computerOption<0.34){
+            computerOption="stone";
+        }else
+        if (computerOption <= 0.67){
+            computerOption ="paper";
+        }else{
+            computerOption ="scissor";
+        }
+        resultMessage = compare(userOption, computerOption);
+        document.getElementById("result").innerHTML ="<p>User select: " + userOption + " - Computer select:"
+            + computerOption + "</p> <p>" + resultMessage + "</p>";
+    }
+    //Function that compares the two selections and return a message.
+    function compare (userSelection , computerSelection)
+    {
+        if (userSelection === undefined)
+        {
+            return "Please, select an option before play.";
+        }
+        if (userSelection === computerSelection)
+        {
+            return "It is a draw !";
+        }
+        if (userSelection === "stone")
+        {
+            if (computerSelection === "scissor")
+            {
+                return "You win.";
+            } else {
+                return "you lose! , Try again! ";
+            }
+        }else if (userSelection === "paper")
+        {
+            if (computerSelection === "stone")
+            {
+                return "You win." ;
+            }else if ("scissor")
+            {
+                return "You lose!, Try again.";
+            }
+        }else if (userSelection === "scissor")
+        {
+            if (computerSelection === "stone")
+            {
+                return "You lose!, Try again.";
+            }else{
+                return "You win." ;
+            }
+        }
+    }
 }
 
